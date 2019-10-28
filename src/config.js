ca1e8bab445ddcfcfc35a32aafa29fb8c04f364a
playdingnow <playdingnow@gmail.com>
2019-10-28 11:28:54 +0800

    doc(sharp): example of format convert
    Signed-off-by: playdingnow <playdingnow@gmail.com>

--- src/config.js
+++ src/config.js
@@ -16,7 +16,11 @@ module.exports = {
       info: {
         title: 'API',
         version: '1.0.0',
-        description: [].join(''),
+        description: [
+          '# base64 image convert',
+          'base64 图片转换',
+          'http://base64image.org/',
+        ].join('\n'),
       },
       tags: [],
       // host: 'localhost:3000',
--- src/controller/sharp.js
+++ src/controller/sharp.js
@@ -45,6 +45,20 @@ const apis = {
       summary: '图片处理API',
       body: {
         type: 'object',
+        example: {
+          image:
+            'UklGRlADAABXRUJQVlA4IEQDAAAQGwCdASqBAHsAP83a52o/tTAorHW7Q/A5iWwA1BJWaE1pZ3bveU5yizgHb7SKLILw+CBCVrqu7bWt7YJFMpxDwHxhG1Gzgb+DQlxYIvqGiLprt9rRbtNtQ1IDDEicmsyWL7foIjMZd1XzLjN9ZEGUUHd6vyQMV5XQBw8Qro41ysLVqeuR3zS2dheapdd2ZfmVDIVR/gNbXnLvajqNzQP6RzFYcXG9+6uQvS8woWeUdsGZWs1zMlccbdCe0NQYQCHiUqvHMq0dWqvFAJaLKac/nZI4Oy3oCREHt6SyAtBXRAAA/u2oAAH99LR7rCbbgcHOJ7qPG9R6n41JtS0bDurgAhjYNrUg1DiS9DNZZ1kWyowfJHPzoKyvthELEIyL03dIXQFZfjse+G46DUQBtR4BBpBWqAYKMHq9MQzK1dTe8/2eeGwoRjvC+BoWNmI/+7sVP8Qtwbj4Fgo7mpjodXfopm8oR4zZmOg2BR+ykhcQydyacv9N3JunwF4JBCZEQH4HozwD6sbNH7TAErTTeNHWyVQAoQC4biHlJPX4YS4QfUw5M9f6El+bE3blgs0NbPJyXTxhem6D5PvTNO5Tpjf9JfuRO99Qqdgip39PMv9yJPPkAANhjuujW1uFVCGxttLsISQyDOin2ujJs26XG00TyOon4BVl5dgqHJPn5fJM0ym3U1T+JzM+97guVbznfbxS6mUh+T6achQCqG3LW/UefYxz19jawMKRMqEoophaQQulTU0Pqo4zC8vyIKzAFs2fqBc+w9JrcwT0tOLXPWZmF5hkjbwHlwFUa6c/p52Db9qWGXutY9ppfwViRchgmPfzQZVPUF9RFob7Ct8ej6yAY9qeChSL6vKTyOqRZGXW4O5eeUGLlTzbgVBS14duDxK0lnEA9N2YQ6l3rIAwdw8DXcSwbhkuo2qfwb5nkSemK+lh0ugypQc53UTCDFQETxY23jaM8oJadYRAceFD2NnTC5Z72Sna/wKIp4Z1cBCFqteX/FnOldnZu9/tts7STWJdA3vOrH9Bfi1LAzTTIKKkP3uk6r6KtwAYxvD4jp7Hk4R6GkWKqnai+kJWDMYJaBum5DBAOrkZqtFWWKAZMaSgaT6AAA==',
+          tasks: [
+            {
+              kind: 'png',
+              args: { quality: 100 },
+            },
+            {
+              kind: 'toBuffer',
+              args: { resolveWithObject: true },
+            },
+          ],
+        },
         properties: {
           image: {
             type: 'string',
